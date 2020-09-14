5239607230b75179adcc6aceac5ac3b4bd2e3601
devyanshuk <devkoirala10@gmail.com>
2020-09-14 21:55:06 +0200

    bubblesort algorithm

---
+++ sorting-algorithms/bubblesort.py
@@ -0,0 +1,7 @@
+def bubbleSort(array):
+    for item in range(len(array) - 1, 0, -1):
+        for i in range(item):
+            if array[i] > array[i + 1]:
+                temp = array[i]
+                array[i] = array[i+1]
+                array[i+1] = temp
