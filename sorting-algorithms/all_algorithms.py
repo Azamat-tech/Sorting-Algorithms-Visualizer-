5d0723e47dc54171e1ec34a2d0c02882d9b209dd
Azamat Zarlykov <azarlykovcz@gmail.com>
2020-09-15 19:10:42 +0200

    add a visualization using pygame

--- sorting-algorithms/all_algorithms.py
+++ sorting-algorithms/all_algorithms.py
@@ -1,6 +1,49 @@
+import pygame
 import bubblesort
 import insertion_sort
 import mergesort
 import selectionsort
 import quicksort
 import heapsort
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0, 0, 128)
+
+class Visualizer:
+
+    HEIGHT = 800
+    WIDTH = 1600
+
+    SIZE = (WIDTH, HEIGHT)
+    TOP_RECTANGLE = (WIDTH, HEIGHT//10)
+
+    def __init__(self):
+        pygame.init()
+        
+        self.screen = pygame.display.set_mode(self.SIZE)
+
+        pygame.display.set_caption("Sorting Visualizer")
+
+        self.clock = pygame.time.Clock()
+
+    def main_loop(self):
+        while True:
+
+            for event in pygame.event.get():
+                pos = pygame.mouse.get_pos()
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+            
+        
+            self.screen.fill(WHITE)
+
+            pygame.draw.rect(self.screen, BLUE, (0, 0, self.WIDTH, self.HEIGHT // 8))
+
+            pygame.display.flip()
+
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    app = Visualizer()
+    app.main_loop()
