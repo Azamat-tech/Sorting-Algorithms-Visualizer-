c9bff1a69fbdc04c2fcc958184bb60491c9fcc38
devyanshuk <devkoirala10@gmail.com>
2020-09-16 17:07:59 +0200

    remove sorting-algorithms dir

--- sorting-algorithms/bubblesort.py
+++
@@ -1,7 +0,0 @@
-def bubbleSort(array):
-    for item in range(len(array) - 1, 0, -1):
-        for i in range(item):
-            if array[i] > array[i + 1]:
-                temp = array[i]
-                array[i] = array[i+1]
-                array[i+1] = temp
