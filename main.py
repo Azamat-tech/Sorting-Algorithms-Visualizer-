7ee235bf6f4d4416fa19b12ef28bf667b7af4eb0
devyanshuk <devkoirala10@gmail.com>
2020-09-16 21:19:32 +0200

    added the ArraySort class

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from view.view import Visualizer
 from sortingalgorithms.all_algorithms import *
+from exceptions.sorting_algorithm_missing import SortingAlgorithmNotFoundError, getErrorMessage
+
+class ArraySort:
+
+    def __init__(self, array):
+        self.sortEvalDictionary = { "bubble" : bubble,
+                                    "insertion" : insertion,
+                                    "merge" : merge,
+                                    "selection" : selection,
+                                    "quick" : quick,
+                                    "heap" : heap
+        }
+        self.array = array
+
+    def sort(self, sortType):
+        a = self.sortEvalDictionary.get(sortType)
+        if not a :
+            print()
+            raise SortingAlgorithmNotFoundError(getErrorMessage(sortType))
+        a.sort(self.array)
+    
+    def __repr__(self):
+        return f"{self.array}"
 
 if __name__ == '__main__':
-    a = [5,4,3,2,1]
-    bubble.sort(a)
+    a = ArraySort([5, 4, 3, 2, 1])
+    a.sort("bubble")
     print(a)
     app = Visualizer()
     app.main_loop()
